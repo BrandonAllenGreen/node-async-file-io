a89b3c0d02f64980c113d7d0f1580816350b03e2
BrandonAllenGreen <green04@gmail.com>
2018-03-05 13:13:42 -0600

    reading and writing with async correctly

--- async.js
+++ async.js
@@ -4,3 +4,17 @@
 const fs = require('fs');
 
 const [,,filePath] = process.argv;
+
+const writeFile = () => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) throw err;
+    process.stdout.write(data + '\n');
+  });
+}
+
+// exit if no argument passed in
+if  (!filePath) {
+  process.exit();
+} else { 
+  writeFile();
+}   
